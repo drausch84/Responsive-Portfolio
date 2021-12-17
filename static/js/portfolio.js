3e4b2e10173f9f2a0d6f98c2b608c921d2a7117d
David <rausch.david84@gmail.com>
2021-12-17 10:58:11 -0700

    Adding initial files to portfolio

---
+++ static/js/portfolio.js
@@ -0,0 +1,23 @@
+//JS for toggling the nav menu between desktop, tablet, and mobil
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='bi bi-list'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='bi bi-x'></i>";
+  }
+}
+
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
